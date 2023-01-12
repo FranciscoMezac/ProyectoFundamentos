83215309fc0db8522d9034af6b82d5364240610e
FranciscoMezac <116356242+FranciscoMezac@users.noreply.github.com>
2023-01-12 08:49:07 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
 import { Character } from "../piton/JavaScript/Character.js/index.js";
+import fs from "./fs";
+
+const prompt = msg => { // magia negra para pedir input
+    fs.writeSync(1, String(msg));
+    let s = '', buf = Buffer.alloc(1);
+    while (buf[0] - 10 && buf[0] - 13)
+        s += buf, fs.readSync(0, buf, 0, 1, 0);
+    return s.slice(1);
+}
 
 function main(){
 
